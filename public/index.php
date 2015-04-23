b604f080b7baa4a65462f9a2ab703ee340663b64
trd-jameshwart <trd.jameshwart@gmail.com>
2015-04-23 14:43:25 +0800

    another commit

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
+   
     <title>Leloo</title>
     <link rel="icon" href="img/icon.png" type="image/x-icon">
 
