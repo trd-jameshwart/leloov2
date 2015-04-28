d3aa2668f63374917d12d45fcf01a06f0592d58c
trd-jameshwart <trd.jameshwart@gmail.com>
2015-04-28 10:38:10 +0800

    add form

--- js/user.js
+++ js/user.js
@@ -22,19 +22,19 @@ $(document).ready(function(){
             $("#reg_error").html("").prepend("<div class='alert alert-danger alert-dismissable'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <strong>Please fill up empty fields</strong></div>");
         }else{
             $.post("public/userAction/register.php",{name:user_name,email:user_email,password:password},function(data){
-              if(data=='0'){
-                  $("#reg_error").html("").fadeIn("<div class='alert alert-danger alert-dismissable'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <strong>Please fill up empty fields</strong></div>");
-              }else{
-                  $("#reg_error").html("").prepend("<div class='alert alert-success alert-dismissable'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <strong>Registration Successful</strong></div>");
-                  $("#frm_regiters input[name='username'] ").val("");
-                  $("#frm_regiters input[name='useremail']").val("");
-                  $("#frm_regiters input[name='userpassword']").val("");
+                if(data=='0'){
+                    $("#reg_error").html("").fadeIn("<div class='alert alert-danger alert-dismissable'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <strong>Please fill up empty fields</strong></div>");
+                }else{
+                    $("#reg_error").html("").prepend("<div class='alert alert-success alert-dismissable'> <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button> <strong>Registration Successful</strong></div>");
+                    $("#frm_regiters input[name='username'] ").val("");
+                    $("#frm_regiters input[name='useremail']").val("");
+                    $("#frm_regiters input[name='userpassword']").val("");
 
-                  setTimeout(function(){
-                      $('#registerModal').fadeOut('slow');
-                  },1000)
+                    setTimeout(function(){
+                        $('#registerModal').fadeOut('slow');
+                    },1000)
 
-              }
+                }
             })
 
         }
@@ -43,26 +43,26 @@ $(document).ready(function(){
     });
 
     $("#user_login").submit(function(e){
-            var user_email = $("#txt_email").val();
-            var password   = $("#txt_password").val();
+        var user_email = $("#txt_email").val();
+        var password   = $("#txt_password").val();
 
-            if(user_email =='' || password ==''){
+        if(user_email =='' || password ==''){
 
-            }else {
-                $.post("public/userAction/login.php",{email:user_email,password:password},function(data){
+        }else {
+            $.post("public/userAction/login.php",{email:user_email,password:password},function(data){
 
-                    if(data=='0'){
-                        alert("empty user name and password");
-                    }else if(data=='1'){
-                        alert("user successfully login");
-                        $("#txt_email").val("");
-                        $("#txt_password").val("");
+                if(data=='0'){
+                    alert("empty user name and password");
+                }else if(data=='1'){
+                    alert("user successfully login");
+                    $("#txt_email").val("");
+                    $("#txt_password").val("");
 
-                    }else{
-                        alert(data);
-                    }
-                });
-            }
+                }else{
+                    alert(data);
+                }
+            });
+        }
 
         e.preventDefault();
     });
