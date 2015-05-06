8dd476372d05331abef5a10367fa1e2ac8632aed
trd-jameshwart <trd.jameshwart@gmail.com>
2015-05-06 10:52:16 +0800

    change all response to json object

--- public/userAction/login.php
+++ public/userAction/login.php
@@ -4,19 +4,22 @@ require_once("../../lib/initialize.php");
 if(isset($_POST['email']) && isset($_POST['password'])){
 
     if(empty($_POST['email']) || empty($_POST['password']) ){
-        echo "0";
+
+        echo json_encode(array("res" =>0));
+
     }else{
-            $user_email = $dbcore->escape_value($_POST['email']);
-            $user_pword = $dbcore->escape_value($_POST['password']);
-            if($user->user_login($user_email,$user_pword)){
+        $user_email = $dbcore->escape_value($_POST['email']);
+        $user_pword = $dbcore->escape_value($_POST['password']);
+        if($user->user_login($user_email,$user_pword)){
 
-                $response = array("res" =>1,
-                    "username"=>$user->user_fullname
-                );
+            $response = array("res" =>1,
+                "username"=>$user->user_fullname
+            );
 
-                echo json_encode($response);
-            }else{
-                echo "incorrect username or password";
-            }
+            echo json_encode($response);
+        }else{
+            echo json_encode(array("res" =>"Incorrect username or password"));
+        }
+
     }
 }
