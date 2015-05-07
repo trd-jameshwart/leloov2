ef414075bcedb4ed40139b0d970e0afcaa5ac453
trd-jameshwart <trd.jameshwart@gmail.com>
2015-05-07 10:24:33 +0800

    resolve fb-root in facebook login

--- public/index.php
+++ public/index.php
@@ -124,8 +124,8 @@ require_once("lib/initialize.php");
         (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
+            js = d.createElement("script");
+            js.id = "fb-root";
             js.src = "//connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
